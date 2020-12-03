c6618e84c5218dac4aab0c6692b88790f2ffc9b4
Federico Blengio <fedeeblengio@gmail.com>
2020-12-02 21:24:05 -0300

    Update header.blade.php

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -12,6 +12,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
 
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+    integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous">
 
     <title>Insumos Inf.</title>
 
